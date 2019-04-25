c10b9729983d82bf6c21a33d57fc5118ddad71ab
Prudence <prudence@blueanchor.io>
2019-04-25 09:11:06 +0200

    Deploy master branch to URL named for pre-prod

--- scripts/deploy_staging_app.sh
+++ scripts/deploy_staging_app.sh
@@ -1,3 +1,3 @@
-echo "{ \"alias\": \"staging-ci-travis-test\" }" > now-staging-app.json
+echo "{ \"alias\": \"staging-ci-travis-pre-prod\" }" > now-staging-app.json
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-staging-app.json
